22779760a7d31aa87575e8f8bf094dcfdc9a037b
masteryi <masteryi@haymoed.com>
2022-07-19 15:51:20 -0400

    Added an installer

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Usage
-- Save the Jiggler service files to systemd folder
-- Modify path of jiggler service files if not saved to /opt/scripts/tinypilot-jiggle/
+Run the configure-jiggler.sh file
+```bash
+./configure-jiggler.sh
+```
---
+++ configure-jiggler.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+mkdir -p /opt/scripts
+systemctl stop jiggler.service
+systemctl stop jiggler.timer
+cp ./jiggler.* /etc/systemd/system/
+cp ./tinypilot-jiggle.bash /opt/scripts/
+systemctl daemon-reload
+systemctl enable jiggler.timer
+echo "Jiggler configuration completed. Please reboot!"
